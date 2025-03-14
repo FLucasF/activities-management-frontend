6a40c4b5af2ca7da1e75d041f21242911c7f504d
FLucasF <lucasfelipegpGITHUB@gmail.com>
2025-03-14 19:46:28 -0300

    adicionando env

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   api: {
-    response: 'https://activities.a4s.dev.br/api/response',
-    user: 'https://activities.a4s.dev.br/api/user'
+    response: 'https://activitiesmngmt.a4s.dev.br/api/response',
+    user: 'https://activitiesmngmt.a4s.dev.br/api/user'
   }
 };
